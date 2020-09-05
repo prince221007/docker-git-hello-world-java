e1c28d2cdacd95e6312a344f782cc0edffed6411
Subhadeep Ghosh <50982209+prince221007@users.noreply.github.com>
2020-09-05 12:38:30 +0530

    checking continious integration between github and docker hub

--- src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,8 +3,8 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping --> version 1 " + i );
+		for(int i = 0 ; i < 50; i++){
+			System.out.println("Good  Ping --> version 1 " + i );
 			Thread.sleep(1000);
 		}
 	}
